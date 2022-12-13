2dcc19ed51673c78990d3fc37697fc2f3aa42873
Menna-shallan <mennatullah.sh@gmail.com>
2022-12-13 15:03:20 -0800

    task 0

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always  (Success)
+ */
+int main(void)
+{
+
+  var = getchar(_putchar);
+  putchar(var, '\n');
+ 
+ 
+  return(0)
+}
+
