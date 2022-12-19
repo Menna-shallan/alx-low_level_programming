c55d754b00c552bcfb2d46663542b0d0097a4752
Menna-shallan <mennatullah.sh@gmail.com>
2022-12-19 15:35:53 -0800

    task 3

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,22 @@
+#include "main.h"
+/**
+ * print_rev - prints string follwed by new line.
+ * @s: string to be printed
+ */
+void print_rev(char *s)
+{
+int a, b;
+
+b = 0;
+
+while (s[n] != '\n')
+	b++;
+
+for (a = b - 1; a >= 0; a--)
+{
+	_putchar(s[a]);
+
+}
+
+_putchar('\n');
+}
