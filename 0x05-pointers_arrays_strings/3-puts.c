f9420d03f6b21d5e258d9b75e433a3afccad94bf
Menna-shallan <mennatullah.sh@gmail.com>
2022-12-20 08:43:46 -0800

    adding

--- 0x05-pointers_arrays_strings/3-puts.c
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -1,13 +1,17 @@
 #include "main.h"
 /**
- * _puts - print a string followed by new line.
+ * _puts - print a string to the stdout
  * @str: the string
  * Return: the length of the string
  */
 void _puts(char *str)
 {
-	while (*str)
-		_putchar(*str++);
+	int a = 0;
 
+	while (str[a] != '\0')
+	{
+		_putchar(str[a]);
+		a++;
+	}
 	_putchar('\n');
 }
