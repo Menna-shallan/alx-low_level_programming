0d5e30ae37cfb37d012203e4a357ee98f979870f
Menna-shallan <mennatullah.sh@gmail.com>
2022-12-23 04:46:30 -0800

    task11

---
+++ 0x06-pointers_arrays_strings/102-magic.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+int main(void)
+
+{
+	int n;
+	int a[5];
+	int *p;
+
+	a[2] = 1024;
+	p = &n;
+	/*
+	 * write  your line of code here..
+	 * main - magic
+	 * you are not allowed to use a
+	 * you are not allowed to modify 
+	 */
+	*(p + 5) = 98;
+	/* this will print 98\n */
+	printf("a[2] = %d\n", a[2]);
+	return (0);
+
+
+
+}
