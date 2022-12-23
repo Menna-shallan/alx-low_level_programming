22024ab1ed12a02e1c7cc9820db78f9b61af8e96
Menna-shallan <mennatullah.sh@gmail.com>
2022-12-23 04:53:58 -0800

    task12

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,4 +20,5 @@ void print_number(int n);
 int main(void);
 void print_number(int n);
 char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_line(char *c, int s, int l);
 #endif 
